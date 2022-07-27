8590168a7b6d2f7f3c3dfd3f2bf090669be7a576
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-07-27 17:14:32 +0300

    update

--- tampermonkey/sber-account.js
+++ tampermonkey/sber-account.js
@@ -7,6 +7,7 @@
 // @match        https://web2.online.sberbank.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sberbank.ru
 // @updateURL    https://raw.githubusercontent.com/nailgun/pta-scripts/master/tampermonkey/sber-account.js
+// @downloadURL  https://raw.githubusercontent.com/nailgun/pta-scripts/master/tampermonkey/sber-account.js
 // @require      https://raw.githubusercontent.com/nailgun/pta-scripts/master/tampermonkey/lib.js?rev=7
 // @grant        GM_registerMenuCommand
 // @grant        GM_log
