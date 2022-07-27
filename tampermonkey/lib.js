bb90aace80f2e49758483a79f198c8e09ce83950
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-07-27 12:03:01 +0300

    update lib

--- tampermonkey/lib.js
+++ tampermonkey/lib.js
@@ -6,11 +6,53 @@
 
     	formatTrs: (trs) => {
 	        let text = `${trs.date} ${trs.name}`;
+            if (trs.time || trs.geo) {
+                text += `  ; ${trs.time} ${trs.geo}`;
+            }
 	        if (trs.src) {
 	            text += `\n    ${trs.src}`;
 	        }
 	        text += `\n    ${trs.dst}  `.padEnd(SUM_INDENT, ' ') + `${trs.sum}\n`;
 	        return text;
     	},
+
+        parseDate: (text) => {
+            let now = new Date();
+            text = text.toLowerCase();
+
+            if (text === 'сегодня') {
+                return now;
+            }
+
+            const MONTH_MAP = {
+                'января': 1,
+                'февраля': 2,
+                'марта': 3,
+                'апреля': 4,
+                'мая': 5,
+                'июня': 6,
+                'июля': 7,
+                'августа': 8,
+                'сентября': 9,
+                'октября': 10,
+                'ноября': 11,
+                'декабря': 12,
+            };
+
+            let match = /^(\d+)\s+(.+)$*/.exec(text);
+            if (!match) {
+                throw new Error('Invalid date: ' + text);
+            }
+
+            return new Date(now.getFullYear(), MONTH_MAP[match[2]], match[1]);
+        },
+
+        formatDate: (date) => {
+            let day = date.getDate().toString().padStart(2, '0');
+            let month = (date.getMonth() + 1).toString().padStart(2, '0');
+            let year = date.getFullYear();
+
+            return `${year}-${month}-${day}`;
+        },
     };
 })();
